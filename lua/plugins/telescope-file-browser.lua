ef67a0969fb5862cf51f86a5f72a247d0984e5c9
Brian <brian@MacBook-Pro-2.lan>
2023-11-14 17:35:50 -0600

    break out plugins

---
+++ lua/plugins/telescope-file-browser.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+
+  config = function()
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>fb",
+      ":Telescope file_browser<CR>",
+      { noremap = true }
+
+    )
+    -- You don't need to set any of these options.
+    -- IMPORTANT!: this is only a showcase of how you can set default options!
+    require("telescope").setup {
+      extensions = {
+        file_browser = {
+          -- disables netrw and use telescope-file-browser in its place
+          hijack_netrw = true,
+        },
+      },
+    }
+    -- To get telescope-file-browser loaded and working with telescope,
+    -- you need to call load_extension, somewhere after setup function:
+    require("telescope").load_extension "file_browser"
+  end
+}
