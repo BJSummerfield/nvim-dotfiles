d3f31527370f1dbeea3c62e1ba5c8f8d87ab2c46
brian <brianjsummerfield@gmail.com>
2023-11-15 22:03:52 -0600

    update colorscheme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     options = {
       icons_enabled = false,
-      theme = 'tokyonight',
+      theme = 'tokyonight-night',
       component_separators = '|',
       section_separators = '',
       globalstatus = true,
