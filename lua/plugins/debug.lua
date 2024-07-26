da3f1a5f3a8c218bd10f246c5f8da9ffa5a24e41
BJSummerfield <brianjsummerfield@gmail.com>
2024-07-26 15:12:37 -0500

    updates

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -56,8 +56,6 @@ return {
     -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
       -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
