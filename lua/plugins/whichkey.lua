2c7145ab9b5c070de6030b2d4e78480d09cc3196
BJSummerfield <brianjsummerfield@gmail.com>
2024-07-26 12:11:03 -0500

    update

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,20 +1,20 @@
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
   config = function() -- This is the function that runs, AFTER loading
     require('which-key').setup()
-
-    -- Document existing key chains
-    require('which-key').register {
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+    require('which-key').add {
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d_', hidden = true },
+      { '<leader>h', group = 'Git [H]unk' },
+      { '<leader>h_', hidden = true },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>t_', hidden = true },
     }
-    -- visual mode
-    require('which-key').register({
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
+    require('which-key').add { { '<leader>h', desc = 'Git [H]unk', mode = 'v' } }
   end,
 }
